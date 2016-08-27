7e68befb17a32e849228ce88ee39160ab2b4bf95
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-08-27 12:06:19 -0600

    insert works + node_links

--- lib/completeme.rb
+++ lib/completeme.rb
@@ -11,32 +11,54 @@ class Trie
   def insert(word, current_node = @root, is_word = false)
 
     # create an array of letters and insert each one individually
-    letters = word.split
+    letters = word.chars
     letters.last << "a"
     letters.each do |letter|
 
       # ^ added a letter to the last letter
       # now if the length is 2, we know we're at the end
-      # chop! the bitch off and set is_word to true
-      is_word = false
+      # chop! that sucker off and set is_word to true
       if letter.length == 2
         letter.chop!
         is_word = true
       end
 
-      if !current_node.instance_variable_get("@#{letter}").nil?
+      # if the link exists
+      if current_node.instance_variable_get("@#{letter}") != nil
 
+        # if it's the last letter, set next node as a word
         if is_word
-          current_node.instance_variable_get("@#{letter}").instance_variable_set("@#{is_word}", true)
+          current_node.instance_variable_get("@#{letter}").is_word = true
         end
 
+      else # if the link does not exist, initialize it
+        current_node.instance_variable_set("@#{letter}", Node.new(is_word))
       end
-      # if link == nil, initialize_link
-      # else view link
-      # should be current_node, not root ******
-      current_node.instance_variable_set("@#{letter}", Node.new(is_word))
 
+      # if it's not the end, look at the next node
+      if is_word == false
+        current_node = current_node.instance_variable_get("@#{letter}")
+      end
     end
+  end
+
+  # accept node argument return an array of it's methods minus :is_word
+  def node_links(current_node)
+
+    links = current_node.instance_variables
+    links.map! do |x|
+      x.to_s.delete!("@")
+    end
+
+    links.shift
+    links.sort
+
+  end
+
+  # accept node argument return all possible words
+  def possible_words(word)
+
+
 
   end
 
--- test/completeme_test.rb
+++ test/completeme_test.rb
@@ -6,20 +6,58 @@ require 'pry'
 class TestCompleteMe < Minitest::Test
 
   def test_insert_the_word_a_root_is_node
-    my_trie = Trie.new
-    my_trie.insert("a")
+    trie = Trie.new
+    trie.insert("a")
 
-    assert_equal Node, my_trie.root.a.class
-    refute my_trie.root.is_word
-    assert my_trie.root.a.is_word
+    assert_equal Node, trie.root.a.class
+    refute trie.root.is_word
+    assert trie.root.a.is_word
   end
 
   def test_insert_two_words_with_same_letters_longest_first
-    my_trie = Trie.new
-    my_trie.insert("am")
-    my_trie.insert("a")
+    trie = Trie.new
+    trie.insert("am")
+    trie.insert("a")
 
-    assert_equal Node, my_trie.root.a.class
+    assert_equal Node, trie.root.a.class
+    assert_equal Node, trie.root.a.m.class
+    assert trie.root.a.is_word
+    assert trie.root.a.m.is_word
+    refute trie.root.m
+  end
+
+  def test_a_handful_of_words
+    trie = Trie.new
+    trie.insert("catapalt")
+    trie.insert("caterpiller")
+    trie.insert("cat")
+    trie.insert("a")
+    trie.insert("ardvark")
+    trie.insert("ark")
+
+    assert_equal Node, trie.root.c.a.t.e.r.p.i.l.l.e.r.class
+    assert trie.root.c.a.t.e.r.p.i.l.l.e.r.is_word
+    assert trie.root.c.a.t.is_word
+    refute trie.root.c.is_word
+    refute trie.root.c.is_word
+
+    assert trie.root.a.is_word
+    assert trie.root.a.r.k.is_word
+    refute trie.root.a.r.is_word
+  end
+
+  def test_node_links
+    trie = Trie.new
+    trie.insert("catapalt")
+    trie.insert("caterpiller")
+    trie.insert("cat")
+    trie.insert("a")
+    trie.insert("ardvark")
+    trie.insert("ark")
+
+    assert_equal trie.node_links(trie.root), ["a", "c"]
+    assert_equal trie.node_links(trie.root.c.a.t), ["a", "e"]
+    assert_equal trie.node_links(trie.root.a.r), ["d", "k"]
   end
 
 end
